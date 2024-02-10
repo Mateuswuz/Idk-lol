d9ecd93d67e3a8e14515d73c552be37a7886bced
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-02-10 15:32:24 -0300

    Add files via upload

---
+++ assets/preload/stages/walter.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('walter', 'walter', -1080, -1050);
+	setScrollFactor('walter', 1, 1);
+	scaleObject('walter', 9.9, 9.9); 
+	addLuaSprite('walter', false);
+end
